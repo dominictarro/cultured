a1accf4502102e32c05bb76ed81522ac0ecabd01
dominictarro <dominic.tarro@gmail.com>
2022-04-16 18:32:46 -0400

    added static folder placeholder and page serving

--- cultured/server.py
+++ cultured/server.py
@@ -1,14 +1,17 @@
 """
 Endpoints for contacting the site.
 """
+from os.path import exists, join
+import traceback
+
 from .logs import logging
 from .state import DailyMemeState, DailyMemeStateEncoder
 from .updater import begin_daily_meme_state_updater
-from flask import request, Flask
+from flask import abort, request, send_from_directory, Flask
 from flask.json import jsonify
 
 logger = logging.getLogger('server')
-app = Flask(__name__)
+app = Flask(__name__, static_folder='front-end')
 app.json_encoder = DailyMemeStateEncoder
 state = DailyMemeState(None, None)
 updater = begin_daily_meme_state_updater(state)
@@ -16,10 +19,17 @@ updater = begin_daily_meme_state_updater(state)
 while state.memes is None:
     ...
 
-@app.route("/")
-def index():
-    logger.info(f"'{request.endpoint}' requested from '{request.remote_addr}' by agent '{request.user_agent}'")
-    return ""
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    try:
+        logger.info(f"'{request.endpoint}' requested from '{request.remote_addr}' by agent '{request.user_agent}'")
+        if path != '' and exists(join(app.static_folder, path)):
+            return send_from_directory(app.static_folder, path)
+        return send_from_directory(app.static_folder, 'index.html')
+    except Exception:
+        logger.error(f"errored when serving {request.endpoint}: {traceback.format_exc()}")
+        abort(500)
 
 @app.route("/today", methods=['GET'])
 def today():
