fc773c7a50048bf81c2c6bb41577cefbb18be79a
dominictarro <dominic.tarro@gmail.com>
2022-04-16 14:31:00 -0400

    server logging initialized

--- cultured/server.py
+++ cultured/server.py
@@ -1,12 +1,13 @@
 """
 Endpoints for contacting the site.
 """
+from .logs import logging
 from .state import DailyMemeState, DailyMemeStateEncoder
 from .updater import begin_daily_meme_state_updater
-from flask import Flask
+from flask import request, Flask
 from flask.json import jsonify
 
-
+logger = logging.getLogger('server')
 app = Flask(__name__)
 app.json_encoder = DailyMemeStateEncoder
 state = DailyMemeState(None, None)
@@ -15,12 +16,12 @@ updater = begin_daily_meme_state_updater(state)
 while state.memes is None:
     ...
 
-@app.route("/index")
 @app.route("/")
 def index():
+    logger.info(f"'{request.endpoint}' requested from '{request.remote_addr}' by agent '{request.user_agent}'")
     return ""
 
-@app.route("/today")
+@app.route("/today", methods=['GET'])
 def today():
+    logger.info(f"'{request.endpoint}' requested from '{request.remote_addr}' by agent '{request.user_agent}'")
     return jsonify(state)
-
