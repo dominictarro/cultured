7c2740c8fce4f9afd888d563af50225abda64a84
dominictarro <dominic.tarro@gmail.com>
2022-04-17 14:44:11 -0400

    dual server bug on test fixed

--- test.sh
+++ test.sh
@@ -4,5 +4,13 @@ source ./venv/bin/activate
 export FLASK_APP=cultured.server
 export FLASK_ENV=development
 
-flask run &
-cd front-end && npm start
+
+# check for flask process
+procs=`ps -eaf | grep ".*/cultured/.*/[f]lask run$"`
+if [ -z "$procs" ]; then
+    # flask process does not exist
+    flask run &
+fi
+echo "Flask already running"
+echo -e "$procs"
+cd front-end && npm start && kill_flask
